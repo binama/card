df3a431da14c75f44bb7fc4164e0436e0018eb53
binama <binama81@gmail.com>
2022-08-24 10:40:44 +0600

    feature: create index and detail page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # card
+склонировать репозиторий командой git clone https://github.com/binama/card.git
+создать виртуальное окружение командой ( python3 -m venv <name_venv> )
+Активировать venv ( source <name_venv>/bin/activate )
+Установить все зависимости проекта из req.txt ( pip install -r req.txt )
+Настроить setting.py ( создать и заполнить файл .env )
+Запустить проект командой python manage.py runserver
---
+++ card/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.CharField(max_length=250, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='post_images')
+
+
+    def __str__(self) -> str:
+        return f'{self.title}->{self.author}'
